7a48d958e7296805be52a1f0bba2731514803288
ctcxiao <1171332103@qq.com>
2018-04-23 09:29:33 +0800

    提交RandomIntGenerator类测试代码

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,31 @@
 package tw.core;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import static junit.framework.TestCase.fail;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
 
+    @Test
+    public void test_generateNums(){
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        try {
+            randomIntGenerator.generateNums(9,4);
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            randomIntGenerator.generateNums(4,9);
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e){
+            Assert.assertEquals("Can't ask for more numbers than are available", e.getMessage());
+        }
+    }
 }
