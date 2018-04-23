b7404be3e43d8476ac06b1efaa7a5a5a34caaa09
ctcxiao <1171332103@qq.com>
2018-04-23 09:39:03 +0800

    提交AnswerGenerator类测试代码

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,24 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static junit.framework.TestCase.fail;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
 
+    @Test
+    public void test_generate(){
+        AnswerGenerator answerGenerator = new AnswerGenerator(new RandomIntGenerator());
+        try {
+            Answer answer = answerGenerator.generate();
+        }catch (OutOfRangeAnswerException e){
+            fail("out of range exception! generate answer failed!");
+        }
+    }
 }
 
