b0305dd39edb27d64903a58b6d94ceaeb7d60ec6
ctcxiao <1171332103@qq.com>
2018-04-23 09:46:26 +0800

    提交GameController类测试

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,8 +1,31 @@
 package tw.controllers;
 
+import com.google.inject.Injector;
+import org.junit.Test;
+import tw.GuessNumberModule;
+import tw.commands.GuessInputCommand;
+
+import java.io.IOException;
+
+import static com.google.inject.Guice.createInjector;
+import static junit.framework.TestCase.fail;
+
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
  */
 public class GameControllerTest {
 
+    @Test
+    public void test_play(){
+        //how to test play with user input,i can not input anything when test running
+
+        
+//        Injector injector = createInjector(new GuessNumberModule());
+//        GameController gameController = injector.getInstance(GameController.class);
+//        try {
+//            gameController.play(new GuessInputCommand());
+//        } catch (IOException e){
+//            fail("test fail!!IOException throw, game play fail");
+//        }
+    }
 }
